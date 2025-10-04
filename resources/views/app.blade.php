38a0dc0c491a7994e7ad70f2a633efd64f816fec
acristhian1411 <acristhian35@gmail.com>
2025-10-04 20:24:16 -0300

    feat(ui): agregar ícono personalizado a la aplicación

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
-    @vite('resources/css/app.css')
-    @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  @vite('resources/js/app.js')
+  @vite('resources/css/app.css')
+  @inertiaHead
+</head>
+
+<body>
+  @inertia
+</body>
+
 </html>
