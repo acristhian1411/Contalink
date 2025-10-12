a6c8f07ed2aa53b3bbae22d0f14efa040b244368
acristhian1411 <acristhian35@gmail.com>
2025-10-12 15:26:42 -0300

    chore(logging): agregar configuración para guardar logs en formato JSON
    - Se actualizó config/logging.php para registrar los logs en formato JSON y facilitar su análisis estructurado.

--- config/logging.php
+++ config/logging.php
@@ -51,7 +51,13 @@ return [
     */
 
     'channels' => [
-
+        'json' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.json.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'formatter' => Monolog\Formatter\JsonFormatter::class,
+            'replace_placeholders' => true,
+        ],
         'stack' => [
             'driver' => 'stack',
             'channels' => explode(',', env('LOG_STACK', 'single')),
@@ -89,7 +95,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
